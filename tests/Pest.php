110fe7587b336f54e8cdcb8a93f5c38b65cf755f
Mateus Getulio Vieira <mateus.getulio@gmail.com>
2026-09-12 14:57:24 -0300

    Remove database scaffolding the prototype does not use

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Pricing\Data\Unit;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 /*
@@ -16,23 +15,7 @@ use Tests\TestCase;
 */
 
 pest()->extend(TestCase::class)
- // ->use(RefreshDatabase::class)
     ->in('Feature');
-
-/*
-|--------------------------------------------------------------------------
-| Expectations
-|--------------------------------------------------------------------------
-|
-| When you're writing tests, you often need to check that values meet certain conditions. The
-| "expect()" function gives you access to a set of "expectations" methods that you can use
-| to assert different things. Of course, you may extend the Expectation API at any time.
-|
-*/
-
-expect()->extend('toBeOne', function () {
-    return $this->toBe(1);
-});
 
 /*
 |--------------------------------------------------------------------------
